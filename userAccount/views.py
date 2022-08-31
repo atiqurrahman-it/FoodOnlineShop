b5de9c642db0bd6430ad9f3b90a3bb24502bd305
atiqur <atiqurrahman.cse2552@gmail.com>
2022-08-31 12:59:45 +0600

    registration update

--- userAccount/views.py
+++ userAccount/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def Register(request):
-    return render(request,'single_page/register-restaurant.html')
+    return render(request,'single_page/registration.html')
